34aacba04882e49ebfaa1a1cf64d205ef3819cb6
Hielke de Lange <Hielke de Lange>
2017-02-08 16:40:57 +0100

    seccond commit

--- template-login.php
+++ template-login.php
@@ -10,16 +10,4 @@
 	kijken of er al is ingelogd
 -->
 
-<?php if(!isset($_SESSION['id'])): ?>
-	<div id="login">
-	<form method="POST">
-		<div>login naam: <input type="text" name="name" /></div>
-		<div>password: <input type="password" name="password" /></div>
-		<div><input type="submit" value="login" /></div>
-	</form>
-	</div>
-
-<?php else: ?>
-
-
-<?php endif ?>
+<?php echo $this->partial("partials/login/login.phtml");
